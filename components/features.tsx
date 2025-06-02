de6bff74795bdf57d9c6c01911658f44881705ce
Malyuk-afk <Qingyuan.Yang21@student.xjtlu.edu.cn>
2025-06-03 02:32:45 +0800

    Update features.tsx

--- components/features.tsx
+++ components/features.tsx
@@ -30,28 +30,6 @@ export default function Features() {
           alt="Blurred shape"
         />
       </div>
-      <div className="mx-auto max-w-6xl px-4 sm:px-6">
-        {/* Section header */}
-        <div className="mx-auto max-w-3xl pb-4 text-center md:pb-12">
-          <h2 className="animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-gray-200),var(--color-indigo-200),var(--color-gray-50),var(--color-indigo-300),var(--color-gray-200))] bg-[length:200%_auto] bg-clip-text pb-4 font-nacelle text-3xl font-semibold text-transparent md:text-4xl">
-            Built for modern product teams
-          </h2>
-          <p className="text-lg text-indigo-200/65">
-            Open AI reads and understands your files, and with nothing more
-            than a single line of feedback, so you can go further than the
-            speed of thought.
-          </p>
-        </div>
-        <div className="flex justify-center pb-4 md:pb-12" data-aos="fade-up">
-          <Image
-            className="max-w-none"
-            src={FeaturesImage}
-            width={1104}
-            height={384}
-            alt="Features"
-          />
-        </div>
-      </div>
     </section>
   );
 }
