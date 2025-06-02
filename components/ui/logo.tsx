2d428ed432737dc2fdf479e1512654a6e2c0d112
Principal-Ideal <119165962+Principal-Ideal@users.noreply.github.com>
2025-06-03 01:23:48 +0800

    Increase logo and text size

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -3,9 +3,9 @@ import logo from "@/public/images/logo.svg";
 
 export default function Logo() {
   return (
-    <div className="inline-flex shrink-0 items-center gap-3 pt-8">
-      <Image src={logo} alt="Logo" width={32} height={32} />
-      <span className="text-sm font-medium text-gray-200">AI-driven tool</span>
+    <div className="inline-flex shrink-0 items-center gap-4 pt-8">
+      <Image src={logo} alt="Logo" width={64} height={64} />
+      <span className="text-xl font-medium text-gray-200">AI-driven tool</span>
     </div>
   );
 }
