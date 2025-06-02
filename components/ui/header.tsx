783bba0c1f4efe9f35358f3c1827a98c6c5bd30f
Principal-Ideal <119165962+Principal-Ideal@users.noreply.github.com>
2025-06-03 01:05:50 +0800

    Remove header dark shade

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -5,7 +5,7 @@ import Logo from "./logo";
 
 export default function Header() {
   return (
-    <header className="z-30 mt-2 w-full md:mt-5">
+    <header className="absolute z-30 w-full">
       <div className="mx-auto max-w-6xl px-4 sm:px-6">
         <div className="flex h-14 items-center">
           <Logo />
