73ea40042e5fb4f16bb21b27b7e073de60714bef
Principal-Ideal <119165962+Principal-Ideal@users.noreply.github.com>
2025-06-03 02:18:05 +0800

    Updated workflows.tsx

--- components/workflows.tsx
+++ components/workflows.tsx
@@ -26,14 +26,14 @@ export default function Workflows() {
             </div>
 
             {/* Spotlight items */}
-            <div className="mt-8 lg:mt-0 lg:w-[580px]" data-aos="fade-up">
+            <div className="mt-8 lg:mt-0 lg:max-w-[580px]" data-aos="fade-up">
               <Spotlight className="group">
                 {/* Card */}
                 <a
-                  className="group/card relative block overflow-hidden rounded-2xl bg-gray-800 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-indigo-500/80 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-indigo-500 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 hover:after:opacity-20 group-hover:before:opacity-100"
+                  className="group/card relative block h-full overflow-hidden rounded-2xl bg-gray-800 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-indigo-500/80 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-indigo-500 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 hover:after:opacity-20 group-hover:before:opacity-100"
                   href="#0"
                 >
-                  <div className="relative z-20 overflow-hidden rounded-[inherit] bg-gray-950 after:absolute after:inset-0 after:bg-linear-to-br after:from-gray-900/50 after:via-gray-800/25 after:to-gray-900/50">
+                  <div className="relative z-20 h-full overflow-hidden rounded-[inherit] bg-gray-950 after:absolute after:inset-0 after:bg-linear-to-br after:from-gray-900/50 after:via-gray-800/25 after:to-gray-900/50">
                     {/* Arrow */}
                     <div
                       className="absolute right-6 top-6 flex h-8 w-8 items-center justify-center rounded-full border border-gray-700/50 bg-gray-800/65 text-gray-200 opacity-0 transition-opacity group-hover/card:opacity-100"
@@ -53,10 +53,10 @@ export default function Workflows() {
                     </div>
                     {/* Image */}
                     <Image
-                      className="w-full"
+                      className="inline-flex"
                       src={WorflowImg01}
-                      width={580}
-                      height={478}
+                      width={350}
+                      height={288}
                       alt="Workflow 01"
                     />
                     {/* Content */}
