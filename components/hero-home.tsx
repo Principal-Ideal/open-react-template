51739d218a76d6f42f075500a997ee5e677da995
Principal-Ideal <119165962+Principal-Ideal@users.noreply.github.com>
2025-06-03 01:33:43 +0800

    Update hero button with spotlight effect

--- components/hero-home.tsx
+++ components/hero-home.tsx
@@ -24,15 +24,15 @@ export default function HeroHome() {
                 Our landing page template works on all devices, so you only have
                 to set it up once, and get beautiful results forever.
               </p>
-              <div className="mx-auto mt-16 flex justify-center">
+              <div className="mx-auto max-w-xs sm:flex sm:max-w-none sm:justify-center">
                 <div data-aos="fade-up" data-aos-delay={400}>
                   <Spotlight>
                     <a
-                      className="group/card relative block overflow-hidden rounded-2xl bg-gray-800 p-px before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-indigo-500/80 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-indigo-500 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 hover:after:opacity-20 group-hover:before:opacity-100"
+                      className="group/card relative block overflow-hidden rounded-2xl bg-gray-800 p-px text-lg before:pointer-events-none before:absolute before:-left-40 before:-top-40 before:z-10 before:h-80 before:w-80 before:translate-x-[var(--mouse-x)] before:translate-y-[var(--mouse-y)] before:rounded-full before:bg-indigo-500/80 before:opacity-0 before:blur-3xl before:transition-opacity before:duration-500 after:pointer-events-none after:absolute after:-left-48 after:-top-48 after:z-30 after:h-64 after:w-64 after:translate-x-[var(--mouse-x)] after:translate-y-[var(--mouse-y)] after:rounded-full after:bg-indigo-500 after:opacity-0 after:blur-3xl after:transition-opacity after:duration-500 hover:after:opacity-20 group-hover:before:opacity-100"
                       href="#0"
                     >
-                      <span className="relative block bg-linear-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] px-12 py-6 text-white shadow-[inset_0px_1px_0px_0px_--theme(--color-white/.16)] hover:bg-[length:100%_150%]">
-                        <span className="relative inline-flex items-center text-xl">
+                      <span className="relative block bg-linear-to-t from-indigo-600 to-indigo-500 bg-[length:100%_100%] bg-[bottom] px-8 py-4 text-white shadow-[inset_0px_1px_0px_0px_--theme(--color-white/.16)] hover:bg-[length:100%_150%]">
+                        <span className="relative inline-flex items-center text-lg">
                           Start Building
                           <span className="ml-2 tracking-normal text-white/50 transition-transform group-hover/card:translate-x-0.5">
                             →
